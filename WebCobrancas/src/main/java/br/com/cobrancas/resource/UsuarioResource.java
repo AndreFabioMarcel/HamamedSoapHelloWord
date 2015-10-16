4f846567769fc31f8806adf4e11a06d9b1efe80c
MarcelMSB <Marcel@192.168.25.24>
2015-10-16 01:08:35 -0300

    Resource de Usuario

--- WebCobrancas/src/main/java/br/com/cobrancas/resource/UsuarioResource.java
+++ WebCobrancas/src/main/java/br/com/cobrancas/resource/UsuarioResource.java
@@ -7,7 +7,10 @@ import br.com.cobrancas.service.UsuarioService;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,6 +25,8 @@ public class UsuarioResource {
     private UsuarioDto.RepresentationBuilder usuarioDtoBuilder;
     @Inject    
     private UsuarioRepository usuarioRepository;
+    @Inject
+    private UsuarioService usuarioService;
     
     @GET
     @Path("{id}")
@@ -39,5 +44,27 @@ public class UsuarioResource {
     public Response findAll(final UsuarioDto dto){
         final List<UsuarioDto> usuarios = usuarioDtoBuilder.toRepresentation(usuarioRepository.findAll());        
         return Response.ok(usuarios).build();
+    }
+    
+    @POST
+    public Response create(final UsuarioDto dto) {
+        final Usuarios usuario = usuarioDtoBuilder.fromRepresentation(dto, Usuarios.Builder.create());
+        usuarioService.persist(usuario);
+        return Response.created(null).entity(usuarioDtoBuilder.toRepresentation(usuario)).build();
+    }
+    
+    @PUT
+    @Path("{id}")
+    public Response update(@PathParam("id") final Long id, final UsuarioDto dto) {
+        final Usuarios usuario = usuarioDtoBuilder.fromRepresentation(dto, Usuarios.Builder.from(
+        usuarioRepository.find(dto.getId())));
+        return Response.created(null).entity(usuarioDtoBuilder.toRepresentation(usuarioService.merge(usuario))).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response remove(@PathParam("id") final Long id) {
+        usuarioService.remove(usuarioRepository.find(id));
+        return Response.noContent().build();
     }
 }
